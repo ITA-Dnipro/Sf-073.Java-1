17286a02b6675ca2b5315f44184b627cf22b81bd
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-12-20 15:50:21 +0200

    Add target and retention to manyToOne and OneToMany

--- src/main/java/org/example/lib/annotations/ManyToOne.java
+++ src/main/java/org/example/lib/annotations/ManyToOne.java
@@ -1,5 +1,14 @@
 package org.example.lib.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(value={METHOD,FIELD})
+@Retention(value=RUNTIME)
 public @interface ManyToOne {
     String columnName() default "";
 } // (MEDIUM)
