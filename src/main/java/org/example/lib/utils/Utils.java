ca5d4582cddd4d88553239691bc47f431cc4d63c
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-12-29 14:50:34 +0200

    Refactor methods setNullToFieldForObject() setValueOfFieldForObject() getValueOfFieldForObject() To use reflection without invoke methods getter/setter
    Add logger to getORMImplementation()
    Change name file in main

--- src/main/java/org/example/lib/utils/Utils.java
+++ src/main/java/org/example/lib/utils/Utils.java
@@ -11,12 +11,9 @@ import org.example.model.Publisher;
 import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.nio.file.Path;
 import java.util.Properties;
 
 @Slf4j
@@ -32,7 +29,13 @@ public class Utils {
                     .getResourceAsStream(filename);
             prop.load(input);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load properties from file: " + filename, e);
+            log.error("Error reading file "+filename+": "+e.getMessage());
+            System.exit(0);
+        }
+
+        if (prop.isEmpty()){
+            log.error("Error reading file "+filename+": property is empty!");
+            System.exit(0);
         }
 
         JdbcDataSource datasource = new JdbcDataSource();
@@ -68,45 +71,65 @@ public class Utils {
     }
 
     public static void setNullToFieldForObject(Object o, Field field) {
-        var methodName = Utils.firstUpperCase(field.getName());
-        var className = o.getClass().getName();
-        try {
-            var methodObject = o.getClass().getDeclaredMethod("set" + Utils.firstUpperCase(field.getName()));
-            methodObject.invoke(o, null);
-        } catch (NoSuchMethodException e) {
-            log.error("Method " + methodName + " for " + className + " is not found! " + e);
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            log.error("Calling " + methodName + " for " + className + " led to an error:" + e);
-        }
+        setValueOfFieldForObject(o, field, null);
     }
 
     public static void setValueOfFieldForObject(Object o, Field field, Object value) {
-        var methodName = Utils.firstUpperCase(field.getName());
-        var className = o.getClass().getName();
+        var className = o.getClass().getSimpleName();
+        field.setAccessible(true);
+        var type = field.getType();
         try {
-            var methodObject = o.getClass().getDeclaredMethod("set" + Utils.firstUpperCase(field.getName()), value.getClass());
-            methodObject.invoke(o, value);
-        } catch (NoSuchMethodException e) {
-            log.error("Method " + methodName + " for " + className + " is not found! " + e);
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            log.error("Calling " + methodName + " for " + className + " led to an error:" + e);
+            if (type == int.class) {
+                field.setInt(o, (int) value);
+            } else if (type == long.class) {
+                field.setLong(o, (long) value);
+            } else if (type == short.class) {
+                field.setShort(o, (short) value);
+            } else if (type == byte.class) {
+                field.setByte(o, (byte) value);
+            } else if (type == float.class) {
+                field.setFloat(o, (float) value);
+            } else if (type == double.class) {
+                field.setDouble(o, (double) value);
+            } else if (type == boolean.class) {
+                field.setBoolean(o, (boolean) value);
+            } else if (type == char.class) {
+                field.setChar(o, (char) value);
+            } else {
+                field.set(o, value);
+            }
+        } catch (IllegalAccessException e) {
+            log.error("An error while setting value for " + field.getName() + " class " + className + "! " + e);
         }
     }
 
     public static Object getValueOfFieldForObject(Object o, Field field) {
-        var methodName = Utils.firstUpperCase(field.getName());
-        var className = o.getClass().getName();
-
+        var className = o.getClass().getSimpleName();
+        field.setAccessible(true);
+        var type = field.getType();
         try {
-            var methodObject = o.getClass().getDeclaredMethod("get" + Utils.firstUpperCase(field.getName()));
-            return methodObject.invoke(o);
-        } catch (NoSuchMethodException e) {
-            log.error("Method " + methodName + " for " + className + " is not found! " + e);
-            return null;
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            log.error("Calling " + methodName + " for " + className + " led to an error:" + e);
-            return null;
+            if (type == int.class) {
+                return field.getInt(o);
+            } else if (type == long.class) {
+                return field.getLong(o);
+            } else if (type == short.class) {
+                return field.getShort(o);
+            } else if (type == byte.class) {
+                return field.getByte(o);
+            } else if (type == float.class) {
+                return field.getFloat(o);
+            } else if (type == double.class) {
+                return field.getDouble(o);
+            } else if (type == boolean.class) {
+                return field.getBoolean(o);
+            } else if (type == char.class) {
+                return field.getChar(o);
+            }
+            return field.get(o);
+        } catch (IllegalAccessException e) {
+            log.error("An error while getting value for " + field.getName() + " class " + className + "! " + e);
         }
+        return null;
     }
 
     public static boolean checkIfObjectInDB(Object o) {
