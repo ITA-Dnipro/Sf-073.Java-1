273b0ad4bc4a19e2ad82e2d21208c8bd14470e29
Gogo Vasev <gvasevg@gmail.com>
2023-01-08 15:16:11 +0200

    unit tests for Utils, updates SQLUtilsTest

---
+++ src/test/java/org/example/lib/utils/DbUtils.java
@@ -0,0 +1,48 @@
+package org.example.lib.utils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DbUtils {
+    public static void clearDatabase(String propertiesFileName) throws SQLException {
+        DataSource datasource = Utils.getDataSourceFromFilename(propertiesFileName);
+
+        Connection c = datasource.getConnection();
+        Statement s = c.createStatement();
+
+        // Disable FK
+        s.execute("SET REFERENTIAL_INTEGRITY FALSE");
+
+        // Find all tables and truncate them
+        Set<String> tables = new HashSet<>();
+        ResultSet rs = s.executeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES  where TABLE_SCHEMA='PUBLIC'");
+        while (rs.next()) {
+            tables.add(rs.getString(1));
+        }
+        rs.close();
+        for (String table : tables) {
+            s.executeUpdate("TRUNCATE TABLE " + table);
+        }
+
+        // Idem for sequences
+        Set<String> sequences = new HashSet<>();
+        rs = s.executeQuery("SELECT SEQUENCE_NAME FROM INFORMATION_SCHEMA.SEQUENCES WHERE SEQUENCE_SCHEMA='PUBLIC'");
+        while (rs.next()) {
+            sequences.add(rs.getString(1));
+        }
+        rs.close();
+        for (String seq : sequences) {
+            s.executeUpdate("ALTER SEQUENCE " + seq + " RESTART WITH 1");
+        }
+
+        // Enable FK
+        s.execute("SET REFERENTIAL_INTEGRITY TRUE");
+        s.close();
+        c.close();
+    }
+}
